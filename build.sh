a3487173a501a4fac5d4f256e174d3398625280e
Katharine Chui <katharine.chui@gmail.com>
2026-05-27 22:09:05 +0200

    keep track of multiple instances of xrGetVulkanDeviceExtensionsKHR

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 set -xe
 
-x86_64-w64-mingw32-gcc -Wformat -fPIC -static -shared -Iminhook_prebuilt/include main.c -Lminhook_prebuilt/bin/ -Wl,-Bdynamic -lMinHook.x64 -o dxgi.dll
+x86_64-w64-mingw32-g++ -Wformat -fPIC -static -shared -Iminhook_prebuilt/include main.cpp -Lminhook_prebuilt/bin/ -Wl,-Bdynamic -lMinHook.x64 -Wl,-Bstatic -lstdc++ -o dxgi.dll
 cp minhook_prebuilt/bin/MinHook.x64.dll ./
--- main.cpp
+++ main.cpp
@@ -15,6 +15,8 @@
 
 #include <wctype.h>
 
+#include <map>
+
 #define LOG_FILE "./beamng_proton_vr_shim.log"
 #define STR(s) #s
 
@@ -50,7 +52,7 @@
 }
 
 #define HOOK_API2(mod_name, name) { \
-	HANDLE lib_handle = LoadLibraryW(mod_name); \
+	HMODULE lib_handle = LoadLibraryW(mod_name); \
 	if (lib_handle == NULL){ \
 		LOG("%s: failed loading library %ls, 0x%x\n", __func__, mod_name, GetLastError()); \
 		exit(1); \
@@ -60,7 +62,7 @@
 		LOG("%s: failed loading %s from %ls\n", __func__, STR(name), mod_name); \
 		exit(1); \
 	} \
-	int create_status = MH_CreateHook(target, name##_hooked, (void **)&name##_orig); \
+	int create_status = MH_CreateHook(target, (void *)name##_hooked, (void **)&name##_orig); \
 	if (create_status != MH_OK){ \
 		LOG("%s: failed creating hook for %ls %s, %d\n", __func__, mod_name, STR(name), create_status); \
 		exit(1); \
@@ -97,13 +99,22 @@ void LOG(const char *fmt, ...){
 }
 
 static HRESULT __stdcall (*CreateDXGIFactory1_real)(REFIID riid, void **ppFactory) = NULL;
+extern "C" {
 HRESULT __stdcall CreateDXGIFactory1(REFIID riid, void **ppFactory){
 	return CreateDXGIFactory1_real(riid, ppFactory);
 }
+}
+
+std::map<void *, void *> xrGetVulkanDeviceExtensionsKHR_map;
 
 // based on the findings of https://github.com/GloriousEggroll/proton-ge-custom/commit/a5a79f5b9d46fb3e02823d8653e91dd12496d3c7
-static int WINAPI (*xrGetVulkanDeviceExtensionsKHR_orig)(void *instance, void *systemId, uint32_t bufferCapacityInput, uint32_t *bufferCountOutput, char *buffer) = NULL;
 int WINAPI xrGetVulkanDeviceExtensionsKHR_hooked(void *instance, void *systemId, uint32_t bufferCapacityInput, uint32_t *bufferCountOutput, char *buffer){
+	auto map_entry = xrGetVulkanDeviceExtensionsKHR_map.find(instance);
+	if (map_entry == xrGetVulkanDeviceExtensionsKHR_map.end()){
+		LOG("%s: xrGetVulkanDeviceExtensionsKHR for instance %p was never registered\n", __func__, instance);
+		exit(1);
+	}
+	int WINAPI (*xrGetVulkanDeviceExtensionsKHR_orig)(void *instance, void *systemId, uint32_t bufferCapacityInput, uint32_t *bufferCountOutput, char *buffer) = (int WINAPI (*)(void *instance, void *systemId, uint32_t bufferCapacityInput, uint32_t *bufferCountOutput, char *buffer))map_entry->second;
 	int result = xrGetVulkanDeviceExtensionsKHR_orig(instance, systemId, bufferCapacityInput, bufferCountOutput, buffer);
 	if (result == 0){
 		LOG("%s: removing openxr required vulkan features\n", __func__);
@@ -112,12 +123,14 @@ int WINAPI xrGetVulkanDeviceExtensionsKHR_hooked(void *instance, void *systemId,
 	return result;
 }
 
-int WINAPI (*xrGetInstanceProcAddr_orig)(void *instance, const char *fn_name, void **out_fn) = NULL;
+
+static int WINAPI (*xrGetInstanceProcAddr_orig)(void *instance, const char *fn_name, void **out_fn) = NULL;
 int WINAPI xrGetInstanceProcAddr_hooked(void *instance, const char *fn_name, void **out_fn){
 	int result = xrGetInstanceProcAddr_orig(instance, fn_name, out_fn);
 	if (result == 0 && strcmp(fn_name, "xrGetVulkanDeviceExtensionsKHR") == 0){
-		xrGetVulkanDeviceExtensionsKHR_orig = *out_fn;
-		*out_fn = xrGetVulkanDeviceExtensionsKHR_hooked;
+		LOG("%s: redirected xrGetInstanceProcAddr_orig for instance %p\n", __func__, instance);
+		xrGetVulkanDeviceExtensionsKHR_map[instance] = *out_fn;
+		*out_fn = (void *)xrGetVulkanDeviceExtensionsKHR_hooked;
 	}
 	return result;
 }
@@ -143,12 +156,12 @@ int init(){
 	char dll_path[1024] = {0};
 
 	sprintf(dll_path, "%s\\system32\\dxgi.dll", win_dir);
-	HANDLE real_dll = LoadLibraryA(dll_path);
+	HMODULE real_dll = LoadLibraryA(dll_path);
 	if (real_dll == NULL){
 		LOG("%s: failed loaing dxgi.dll, 0x%x\n", __func__, GetLastError());
 		exit(1);
 	}
-	CreateDXGIFactory1_real = (void *)GetProcAddress(real_dll, "CreateDXGIFactory1");
+	CreateDXGIFactory1_real = (HRESULT __stdcall (*)(REFIID riid, void **ppFactory))GetProcAddress(real_dll, "CreateDXGIFactory1");
 	if (CreateDXGIFactory1_real == NULL){
 		LOG("%s: failed fetching CreateDXGIFactory1\n", __func__);
 		exit(1);
